91351557b67730eac9029202af3bfe7995fe6ba8
hk-cogoport <harshit.kumar@cogoport.com>
2023-08-09 00:42:31 +0530

    integrated-profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  has_one_attached :avatar
 
 
   before_create :randomize_id
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     get '/users/password', to: 'devise/passwords#new'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:show]
 
   get 'home/about'
 
