726a966613611869efc478222ab4f439695cf405
hk-cogoport <harshit.kumar@cogoport.com>
2023-08-08 16:44:28 +0530

    user-post-integration

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,11 @@ class Post < ApplicationRecord
   before_create :randomize_id
 
 
-  
+
   private
   def randomize_id
     begin
-      self.id = SecureRandom.random_number(1_000_000_000)
+      self.id = SecureRandom.uuid
     end while User.where(id: self.id).exists?
   end
 end
